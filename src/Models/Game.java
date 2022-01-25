6ea929167fffe663852e664cc4d19d47072e6287
MohamedSaidCS <60986229+MohamedSaidCS@users.noreply.github.com>
2022-01-25 11:09:12 +0200

    Added Models

---
+++ src/Models/Game.java
@@ -0,0 +1,26 @@
+package Models;
+
+public class Game {
+    private long id;
+    private long winnerID;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getWinnerID() {
+        return winnerID;
+    }
+
+    public void setWinnerID(long winnerID) {
+        this.winnerID = winnerID;
+    }
+
+//    public Game findByID(long id) {
+//
+//    }
+}
