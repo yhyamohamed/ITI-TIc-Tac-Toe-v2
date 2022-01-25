15a4d3bb6bfb8ed3a666b79fcb8820012f069683
Muhammed Ismail <muhammadahmed.free@gmail.com>
2022-01-25 12:21:41 +0200

    Adding Bot controllers

--- src/ui_modules/WinningLine.java
+++ src/ui_modules/WinningLine.java
@@ -2,12 +2,18 @@ package ui_modules;
 
 import javafx.animation.*;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Line;
 
 import javafx.util.Duration;
 
 public class WinningLine {
     private Line line;
+    private BorderPane root;
+    public WinningLine(){
+        line = new Line();
+    }
     public WinningLine(BorderPane root,BoardGridPane boardGridPane){
         line = new Line();
         line.setStartX((boardGridPane.getTiles()[0][0]).getTranslateX()+100);
@@ -24,5 +30,33 @@ public class WinningLine {
                 new KeyValue(line.endYProperty(),centerY)));
         root.getChildren().add(line);
         tLine.play();
+    }
+    public WinningLine(BorderPane root, StackPane firstTile, StackPane middleTile, StackPane lastTile)
+    {
+        this.root=root;
+        line = new Line();
+        int X=50;
+        int Y=150;
+        int d=100;
+        line.setStartX(X+d*GridPane.getColumnIndex(firstTile));
+        line.setStartY(Y+d*GridPane.getRowIndex(firstTile));
+        line.setEndX(X+d*GridPane.getColumnIndex(lastTile));
+        line.setEndY(Y+d*GridPane.getRowIndex(lastTile));
+        double centerX = X+d*GridPane.getColumnIndex(firstTile);
+        double centerY= Y+d*GridPane.getRowIndex(firstTile);
+        /*System.out.println("line");
+        Timeline tLine = new Timeline();
+
+        tLine.getKeyFrames().add(new KeyFrame(Duration.seconds(20),
+                new KeyValue(line.endXProperty(),centerX),
+                new KeyValue(line.endYProperty(),centerY)));
+                tLine.play();
+*/
+        root.getChildren().add(line);
+
+    }
+    public void removeThisLine()
+    {
+        root.getChildren().remove(line);
     }
 }
