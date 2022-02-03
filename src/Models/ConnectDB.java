dc89101b38662296503a876822ca29b049d01b26
Hegabovic <56083879+Hegabovic@users.noreply.github.com>
2022-02-03 18:13:29 +0200

    Added AppConfig.java to handle application control from only one file

--- src/Models/ConnectDB.java
+++ src/Models/ConnectDB.java
@@ -1,14 +1,19 @@
 package Models;
 
+import application.AppConfig;
+import jdk.nashorn.internal.runtime.regexp.joni.Config;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 class ConnectDB {
+    String databaseName = AppConfig.databaseName;
+    String databasePassword = AppConfig.databasePassword;
 
     public Connection getConnection() throws SQLException {
 
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/tik_tak_tok", "root", "");
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/tik_tak_tok", databaseName, databasePassword);
 
     }
 
