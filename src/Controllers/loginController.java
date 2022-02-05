22462768bb09d4509e22bfb31936a26b5c528125
RanaEhab56 <71533484+RanaEhab56@users.noreply.github.com>
2022-02-05 14:24:06 +0200

    pop up when server is off

--- src/Controllers/loginController.java
+++ src/Controllers/loginController.java
@@ -6,6 +6,10 @@ import com.google.gson.JsonObject;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import ui_modules.Home;
 import ui_modules.SignUp;
@@ -14,8 +18,8 @@ import ui_modules.logIn;
 public class loginController {
     private logIn loginPage;
     private HomePageController homePageController;
-    static boolean validUser= false;
-   
+    static boolean validUser = false;
+
     //private ServerConnector serverConnector;
 
     public loginController(logIn logInPage, Stage primaryStage) {
@@ -33,7 +37,7 @@ public class loginController {
             @Override
             public void handle(ActionEvent event) {
                 homePageController = new HomePageController(primaryStage);
-                homePageController.loadGameBoard(primaryStage, true,false,false);
+                homePageController.loadGameBoard(primaryStage, true, false, false);
             }
         };
     }
@@ -51,36 +55,56 @@ public class loginController {
             }
         };
     }
-// create json object : - client -
+
+    // create json object : - client -
     private EventHandler<ActionEvent> logIn(Stage primaryStage) {
         return new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 Player player = new Player();
-                if (loginPage.getUserNameTxt() != null && loginPage.getPasswordTxt() != null) {
-                    //validUser = player.checkLogin(loginPage.getUserNameTxt(), loginPage.getPasswordTxt());
-                    String response=ServerConnector.signIn(loginPage.getUserNameTxt(),loginPage.getPasswordTxt());
-                    if (response.equals("true")) {
-                        validUser=true;
-                        Home root = new Home(primaryStage);
-                        Scene scene = new Scene(root);
-                        primaryStage.setTitle("home screen");
-                        primaryStage.setScene(scene);
-                        primaryStage.show();
-                        primaryStage.setOnCloseRequest((e)->{
-                            JsonObject closingObj = new JsonObject();
-                            closingObj.addProperty("type","client_close");
-                            closingObj.addProperty("username",ServerConnector.PlayerInfo.username);
-                            ServerConnector.close(closingObj);
-                        });
+                try {
+                    if (loginPage.getUserNameTxt() != null && loginPage.getPasswordTxt() != null) {
+                        //validUser = player.checkLogin(loginPage.getUserNameTxt(), loginPage.getPasswordTxt());
+                        String response = ServerConnector.signIn(loginPage.getUserNameTxt(), loginPage.getPasswordTxt());
 
-                    } else {
-                        validUser=false;
-                        loginPage.getLogInMsg().setVisible(true);
+                        if (response.equals("true")) {
+                            validUser = true;
+                            Home root = new Home(primaryStage);
+                            Scene scene = new Scene(root);
+                            primaryStage.setTitle("home screen");
+                            primaryStage.setScene(scene);
+                            primaryStage.show();
+                            primaryStage.setOnCloseRequest((e) -> {
+                                JsonObject closingObj = new JsonObject();
+                                closingObj.addProperty("type", "client_close");
+                                closingObj.addProperty("username", ServerConnector.PlayerInfo.username);
+                                ServerConnector.close(closingObj);
+                            });
+
+                        } else {
+                            validUser = false;
+                            loginPage.getLogInMsg().setVisible(true);
+                        }
                     }
+
+                } catch (Exception e) {
+//                    e.printStackTrace();
+
+                    Alert alert = new Alert(Alert.AlertType.WARNING, "Connnection failed",  ButtonType.OK);
+
+                    alert.getDialogPane().setMinHeight(100);
+                    alert.getDialogPane().setMinWidth(100);
+                    alert.setResizable(false);
+                    alert.setTitle("Connection");
+                    alert.initOwner(primaryStage);
+                    alert.show();
                 }
             }
-        };
-    }
 
+
+
+
+        };
+
+    }
 }
